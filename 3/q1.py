bd941fb62c4ca96f8eec41e10060f03761ea3975
Suriya Chaubey <64011643@kmitl.ac.th>
2023-12-28 14:43:08 +0700

    rename

---
+++ 3/q1.py
@@ -0,0 +1,53 @@
+import turtle
+
+class Disk(object):
+    def __init__(self, name="", xpos=0, ypos=0, height=20, width=20):
+        self.dname = name
+        self.dxpos = xpos
+        self.dypos = ypos
+        self.dheight = height
+        self.dwidth = width
+        self.t = turtle.Turtle()
+    
+    def showdisk(self):
+        self.t.penup()
+        self.t.setpos(self.dxpos, self.dypos)
+        self.t.pendown()
+        
+        self.t.setheading(0)
+        self.t.forward(self.dwidth/2)
+        self.t.left(90)
+        self.t.forward(self.dheight)
+        self.t.left(90)
+        self.t.forward(self.dwidth)
+        self.t.left(90)
+        self.t.forward(self.dheight)
+        self.t.left(90)
+        self.t.forward(self.dwidth/2)
+        self.t.setheading(0)
+    
+    def newpos(self, xpos, ypos):
+        self.dxpos = xpos
+        self.dypos = ypos
+        
+        self.t.penup()
+        self.t.setpos(self.dxpos, self.dypos)
+        self.t.pendown()
+        
+    def cleardisk(self):
+        self.t.penup()
+        self.t.setpos(self.dxpos, self.dypos)
+        self.t.pendown()
+        self.t.color("white")
+        self.t.setheading(0)
+        self.t.forward(self.dwidth/2)
+        self.t.left(90)
+        self.t.forward(self.dheight)
+        self.t.left(90)
+        self.t.forward(self.dwidth)
+        self.t.left(90)
+        self.t.forward(self.dheight)
+        self.t.left(90)
+        self.t.forward(self.dwidth/2)
+        self.t.setheading(0)
+        self.t.color("black")
