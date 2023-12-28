314e8a5dcc02a3812979fdbb38e06136c0648595
Suriya Chaubey <64011643@kmitl.ac.th>
2023-12-28 14:07:26 +0700

    main edit

--- 1.2/main.py
+++ 1.2/main.py
@@ -1,7 +1,24 @@
 from drawing1 import Simple_drawing_window1
 from topp_q2 import Simple_drawing_window2
-from q3 import Simple_drawing_window3
+from  q3 import Simple_drawing_window3
+
+import sys
+from PySide6.QtCore import *
+from PySide6.QtGui import *
+from PySide6.QtWidgets import *
+
+def main():
+    app = QApplication(sys.argv)
+    w = Simple_drawing_window1()
+    r = Simple_drawing_window2()
+    t = Simple_drawing_window3()
+    
+    r.show()
+    w.show()
+    t.show()
+
+    return app.exec()
 
 
 if __name__ == "__main__":
-    
+    sys.exit(main())
