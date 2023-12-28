0062880eed2bb0f822a6801a9b2f878e35eb3349
Zoro <145021198+RoronoaZoro321@users.noreply.github.com>
2023-12-28 14:05:09 +0700

    1-3

--- 1.2/main.py
+++ 1.2/main.py
@@ -1,6 +1,6 @@
 from drawing1 import Simple_drawing_window1
 from topp_q2 import Simple_drawing_window2
-from  3 import Simple_drawing_window3
+from q3 import Simple_drawing_window3
 
 
 if __name__ == "__main__":
