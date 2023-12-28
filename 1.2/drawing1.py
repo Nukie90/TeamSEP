8af17f5dace4aa80074df18b54793772e2cafcbf
Intutch Sribanyaranond <65011306@kmitl.ac.th>
2023-12-28 14:08:42 +0700

    fix path

--- 1.2/drawing1.py
+++ 1.2/drawing1.py
@@ -7,7 +7,7 @@ class Simple_drawing_window1(QWidget):
     def __init__(self):
         QWidget.__init__(self, None)
         self.setWindowTitle("Simple Drawing")
-        self.rabbit = QPixmap("images/rabbit.png")
+        self.rabbit = QPixmap("1.2/images/rabbit.png")
         
     def paintEvent(self, e):
         p = QPainter()
